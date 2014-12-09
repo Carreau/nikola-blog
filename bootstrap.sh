35f3bb2cc0b12940fa7d38a7eadad1b847e03b89
Bussonnier Matthias <bussonniermatthias@gmail.com>
2014-12-09 18:07:04 +0100

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-nikola install_theme zen-ipython
+# don't need to install as modified theme is bundeled
+# nikola install_theme zen-ipython
+
 git clone ssh://github/carreau/posts posts
 git clone ssh://github/carreau/carreau.github.io output
