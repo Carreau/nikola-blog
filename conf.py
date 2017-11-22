1b78b65cade457906840d9cfe58a8f0d851f59fe
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-11-22 23:41:33 +0100

    inject GA script II

--- conf.py
+++ conf.py
@@ -537,15 +537,7 @@ SOCIAL_BUTTONS_CODE = ""
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 BODY_END = """
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110089201-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-110089201-1');
-</script>
+SEE BASE.TMPL
 """
 
 # The possibility to extract metadata from the filename by using a
